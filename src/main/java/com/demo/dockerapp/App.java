a109506e97a49924c44728d4417ba68b4f8766d6
sameera <sameera.mahajan.85@gmail.com>
2017-08-24 17:25:52 +0530

    First commit

---
+++ src/main/java/com/demo/dockerapp/App.java
@@ -0,0 +1,13 @@
+package com.demo.mvndockerapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
